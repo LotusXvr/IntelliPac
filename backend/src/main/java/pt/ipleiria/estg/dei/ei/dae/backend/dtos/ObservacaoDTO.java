ab9ac80ec81209641a45ed98c66049850c8b69de
Diogo Guerra <2211027@my.ipleiria.pt>
2024-01-02 17:15:14 +0000

    Correçao de observacao

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/dtos/ObservacaoDTO.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/dtos/ObservacaoDTO.java
@@ -6,17 +6,13 @@ public class ObservacaoDTO implements Serializable {
 
     private String valor;
     private Long sensorId;
-    private Long produtoFisicoId;
-    private Long embalagemId;
 
     public ObservacaoDTO() {
     }
 
-    public ObservacaoDTO(String valor, Long sensorId, Long produtoFisicoId, Long embalagemId) {
+    public ObservacaoDTO(String valor, Long sensorId) {
         this.valor = valor;
         this.sensorId = sensorId;
-        this.produtoFisicoId = produtoFisicoId;
-        this.embalagemId = embalagemId;
     }
 
     public String getValor() {
@@ -35,19 +31,4 @@ public class ObservacaoDTO implements Serializable {
         sensorId = sensorId;
     }
 
-    public Long getProdutoFisicoId() {
-        return produtoFisicoId;
-    }
-
-    public void setProdutoFisicoId(Long produtoFisicoId) {
-        this.produtoFisicoId = produtoFisicoId;
-    }
-
-    public Long getEmbalagemId() {
-        return embalagemId;
-    }
-
-    public void setEmbalagemId(Long embalagemId) {
-        this.embalagemId = embalagemId;
-    }
 }
