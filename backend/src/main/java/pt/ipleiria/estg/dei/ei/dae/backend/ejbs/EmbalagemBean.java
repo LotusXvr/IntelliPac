9bf676ecdae1096b059ff424f9844eb258da674b
Lucas <2211034@my.ipleiria.pt>
2024-01-12 14:54:33 +0000

    embalagens lazy load sensores

--- backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/ejbs/EmbalagemBean.java
+++ backend/src/main/java/pt/ipleiria/estg/dei/ei/dae/backend/ejbs/EmbalagemBean.java
@@ -3,6 +3,7 @@ package pt.ipleiria.estg.dei.ei.dae.backend.ejbs;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.hibernate.Hibernate;
 import pt.ipleiria.estg.dei.ei.dae.backend.dtos.EmbalagemDTO;
 import pt.ipleiria.estg.dei.ei.dae.backend.entities.Embalagem;
 import pt.ipleiria.estg.dei.ei.dae.backend.entities.Sensor;
@@ -27,10 +28,63 @@ public class EmbalagemBean {
         return entityManager.createNamedQuery("getAllEmbalagem", Embalagem.class).getResultList();
     }
 
+    public Embalagem findEmbalagemDetails(long id) throws MyEntityNotFoundException {
+        Embalagem embalagem = entityManager.find(Embalagem.class, id);
+        if (embalagem != null) {
+            Hibernate.initialize(embalagem.getSensores());
+        }
+
+        return embalagem;
+    }
+
     public Embalagem create(String material) {
         Embalagem embalagem = new Embalagem(material);
         entityManager.persist(embalagem);
         return embalagem;
+    }
+
+    public Embalagem update(long id, String material) throws MyEntityNotFoundException {
+        Embalagem embalagem = find(id);
+        if (embalagem == null) {
+            throw new MyEntityNotFoundException("Embalagem with id " + id + " not found");
+        }
+        embalagem.setMaterial(material);
+        entityManager.merge(embalagem);
+        return embalagem;
+    }
+
+    public void delete(long id) throws MyEntityNotFoundException {
+        Embalagem embalagem = find(id);
+        if (embalagem == null) {
+            throw new MyEntityNotFoundException("Embalagem with id " + id + " not found");
+        }
+        entityManager.remove(embalagem);
+    }
+
+    public void associateSensorToEmbalagem(long idEmbalagem, long idSensor) throws MyEntityNotFoundException {
+        Embalagem embalagem = find(idEmbalagem);
+        if (embalagem == null) {
+            throw new MyEntityNotFoundException("Embalagem with id " + idEmbalagem + " not found");
+        }
+        Sensor sensor = entityManager.find(Sensor.class, idSensor);
+        if (sensor == null) {
+            throw new MyEntityNotFoundException("Sensor with id " + idSensor + " not found");
+        }
+        embalagem.addSensor(sensor);
+        entityManager.merge(embalagem);
+    }
+
+    public void removeSensorFromEmbalagem(long idEmbalagem, long idSensor) throws MyEntityNotFoundException {
+        Embalagem embalagem = find(idEmbalagem);
+        if (embalagem == null) {
+            throw new MyEntityNotFoundException("Embalagem with id " + idEmbalagem + " not found");
+        }
+        Sensor sensor = entityManager.find(Sensor.class, idSensor);
+        if (sensor == null) {
+            throw new MyEntityNotFoundException("Sensor with id " + idSensor + " not found");
+        }
+        embalagem.removeSensor(sensor);
+        entityManager.merge(embalagem);
     }
 
 }
