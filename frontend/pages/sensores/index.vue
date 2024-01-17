952bcb18dbd8b960bc82ad7bc49021583363f597
Lucas <2211034@my.ipleiria.pt>
2024-01-17 13:15:52 +0000

    feature: divisao dos sensores por estado

--- frontend/pages/sensores/index.vue
+++ frontend/pages/sensores/index.vue
@@ -13,7 +13,38 @@
                 <th>Estado</th>
                 <th>Ações</th>
             </tr>
-            <tr v-for="sensor in sensores">
+            <h4>Sensores Disponiveis</h4>
+            <tr v-for="sensor in sensoresDisponiveis">
+                <td>{{ sensor.idSensor }}</td>
+                <td>{{ sensor.tipo }}</td>
+                <td>{{ sensor.unidade }}</td>
+                <td>{{ sensor.valor }}</td>
+                <td>{{ sensor.estado }}</td>
+                <td>
+                    <nuxt-link :to="`/sensores/${sensor.id}`">Detalhes</nuxt-link>
+                    |
+                    <nuxt-link :to="'/sensores/edit/' + sensor.id">Editar</nuxt-link>
+                    |
+                    <button @click="deleteProduto(sensor.id)">Excluir</button>
+                </td>
+            </tr>
+            <h4>Sensores em Uso</h4>
+            <tr v-for="sensor in sensoresEmUso">
+                <td>{{ sensor.idSensor }}</td>
+                <td>{{ sensor.tipo }}</td>
+                <td>{{ sensor.unidade }}</td>
+                <td>{{ sensor.valor }}</td>
+                <td>{{ sensor.estado }}</td>
+                <td>
+                    <nuxt-link :to="`/sensores/${sensor.id}`">Detalhes</nuxt-link>
+                    |
+                    <nuxt-link :to="'/sensores/edit/' + sensor.id">Editar</nuxt-link>
+                    |
+                    <button @click="deleteProduto(sensor.id)">Excluir</button>
+                </td>
+            </tr>
+            <h4>Sensores de Produto</h4>
+            <tr v-for="sensor in sensoresProduto">
                 <td>{{ sensor.idSensor }}</td>
                 <td>{{ sensor.tipo }}</td>
                 <td>{{ sensor.unidade }}</td>
@@ -39,6 +70,18 @@ import Navbar from "~/layouts/nav-bar.vue"
 const config = useRuntimeConfig()
 const api = config.public.API_URL
 const { data: sensores, error, refresh } = await useFetch(`${api}/sensores`)
+console.log(sensores)
+
+// sensores que estiverem com estado a 2 significa que são de produto (não reutilizáveis)
+const sensoresProduto = sensores.value.filter((sensor) => sensor.estado == 2)
+
+// sensores com estado a 0 significa que são reutilizáveis e estão disponíveis
+const sensoresDisponiveis = sensores.value.filter((sensor) => sensor.estado == 0)
+console.log("sensores disponiveis " + sensoresDisponiveis)
+
+// sensores com estado a 1 significa que são reutilizáveis e estão em uso
+const sensoresEmUso = sensores.value.filter((sensor) => sensor.estado == 1)
+
 
 const deleteProduto = async (id) => {
     try {
